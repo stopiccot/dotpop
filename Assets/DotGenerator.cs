043b8681df54ad76bc9033577393d5e7e0ece0d8
stopiccot <alexey.petruchik@gmail.com>
2018-06-29 21:41:21 +0300

    dotpop

---
+++ Assets/DotGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotGenerator : MonoBehaviour {
+
+	public RectTransform dotPrefab;
+
+	void Start () {
+		for (int i = 0; i < 10000; i++) {
+			var dot = Instantiate(dotPrefab, this.transform);
+			dot.gameObject.name = $"dot{i}";
+			dot.anchoredPosition = new Vector2(Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f));
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/DotScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DotScript : MonoBehaviour {
+
+	protected Vector2 speed;
+	protected RectTransform rectTransform;
+    
+	// Use this for initialization
+	void Start () {
+		rectTransform = GetComponent<RectTransform>();
+		speed = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+		speed = speed / speed.magnitude;
+		GetComponent<Image>().color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rectTransform.anchoredPosition = rectTransform.anchoredPosition + 50 * speed * Time.deltaTime;
+
+		const float fieldSize = 100;
+
+		if (Mathf.Abs(rectTransform.anchoredPosition.x) > fieldSize) {
+			speed.x = -speed.x;
+		}
+
+		if (Mathf.Abs(rectTransform.anchoredPosition.y) > fieldSize) {
+            speed.y = -speed.y;
+        }
+
+		rectTransform.anchoredPosition = new Vector2(Mathf.Clamp(rectTransform.anchoredPosition.x, -fieldSize, fieldSize), Mathf.Clamp(rectTransform.anchoredPosition.y, -fieldSize, fieldSize));
+	}
+}
