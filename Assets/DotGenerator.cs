4e7c7e6889cea14809a4e7283f9346492aad2e60
stopiccot <alexey.petruchik@gmail.com>
2018-07-03 16:33:48 +0300

    wip

--- Assets/DotGenerator.cs
+++ Assets/DotGenerator.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class DotGenerator : MonoBehaviour {
 
 	public RectTransform dotPrefab;
+	protected List<DotScript> dots = new List<DotScript>();
 
-	void Start () {
-		for (int i = 0; i < 10000; i++) {
+	void Start() {
+		for (int i = 0; i < 300; i++) {
 			var dot = Instantiate(dotPrefab, this.transform);
 			dot.gameObject.name = $"dot{i}";
 			dot.anchoredPosition = new Vector2(Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f));
+			dots.Add(dot.GetComponent<DotScript>());
 		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+		for (int i = 0; i < dots.Count; i++) {
+			if (dots[i].popping) {
+				continue;
+			}
+
+			for (int j = 0; j < dots.Count; j++) {
+				if (!dots[j].popping || i == j || dots[j].scale < 0.001f) {
+					continue;
+				}
+
+				var d = dots[i].rectTransform.anchoredPosition - dots[j].rectTransform.anchoredPosition;
+				if (d.magnitude < dots[i].defaultR + dots[i].defaultR * dots[j].scale) {
+					dots[i].Pop();
+					break;
+				}
+			}
+		}
+	}
+
+	public void ButtonClick() {
+		dots[0].Pop();
 	}
 }
--- Assets/DotScript.cs
+++ Assets/DotScript.cs
@@ -5,20 +5,36 @@ using UnityEngine.UI;
 
 public class DotScript : MonoBehaviour {
 
+	public AnimationCurve popCurve;
+	public float popDuration;
+	public float defaultR;
+
 	protected Vector2 speed;
-	protected RectTransform rectTransform;
+	public RectTransform rectTransform { get; protected set; }
+
+	public bool popping = false;
+	public float scale = 1.0f;
+	public float popTime = 0.0f;
     
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		rectTransform = GetComponent<RectTransform>();
+		rectTransform.localScale = 2 * defaultR * Vector3.one;
 		speed = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
 		speed = speed / speed.magnitude;
-		GetComponent<Image>().color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
+		GetComponent<Image>().color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);      
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		rectTransform.anchoredPosition = rectTransform.anchoredPosition + 50 * speed * Time.deltaTime;
+	void Update() {
+		if (popping) {
+			popTime += Time.deltaTime;
+			scale = popCurve.Evaluate(popTime / popDuration);
+			rectTransform.localScale = 2 * defaultR * scale * Vector3.one;
+			return;
+		}
+
+		rectTransform.anchoredPosition = rectTransform.anchoredPosition + 60 * speed * Time.deltaTime;
 
 		const float fieldSize = 100;
 
@@ -31,5 +47,11 @@ public class DotScript : MonoBehaviour {
         }
 
 		rectTransform.anchoredPosition = new Vector2(Mathf.Clamp(rectTransform.anchoredPosition.x, -fieldSize, fieldSize), Mathf.Clamp(rectTransform.anchoredPosition.y, -fieldSize, fieldSize));
+	}
+
+	public void Pop() {
+		speed = new Vector2(0, 0);
+		popping = true;
+		popTime = 0.0f;
 	}
 }
